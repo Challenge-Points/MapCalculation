d05766523e1d2e81e6599c7e2137a8b3563ca3a0
TopKekJordi <49204961+LUIDWIGI@users.noreply.github.com>
2021-02-25 17:14:35 +0100

    Added comments
    Other people may now understand what the fuck is going on.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from functionlib import *
 
 print('--------------')
-
-score = int(input('Score: '))
+ 
+#Takes the inputs.
+score = int(input('Score: ')) 
 mods = input('modifiers: ').split(', ')
 notes = int(input('notes: '))
 weight = float(input('weight: '))
 
-pog = modToRaw(score, mods)
-acc = scoreToAcc(notes, pog)
-cp = getScore(0, acc, weight)
+pog = modToRaw(score, mods) #Takes your score and removes any modifiers.
+acc = scoreToAcc(notes, pog) #Takes score and puts it into a accuracy percentage (can be > 100).
+cp = getScore(0, acc, weight) # Takes score and calculates your CP (points).
 
-print('--------------')
-print(f'acc = {acc}')
-print(f'cp = {cp}')
+
+print('--------------') 
+print(f'acc = {acc}') #Tells you your accuracy.
+print(f'cp = {cp}') #Tells you your CP.
